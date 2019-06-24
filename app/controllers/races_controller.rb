4077925c806cf9ff3737150a59275410bed85912
hansenjl <jenn.leigh.hansen@gmail.com>
2019-06-24 14:53:19 -0700

    Add controller actions to Race Controller

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,37 @@
 class RacesController < ApplicationController
+
+  def index
+    @races = current_user.races
+  end
+
+  def show
+    @race = Race.find_by(params[:id])
+  end
+
+
+  def new
+    @race = Race.new
+  end
+
+  def create
+    @race = current_user.races.build(race_params)
+    if @race.save
+      redirect_to race_path(@race)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def race_params
+    params.require(:race).permit(:title, :distance, :category)
+  end
+
 end
